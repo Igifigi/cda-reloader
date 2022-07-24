078da58c6842a805ba71039ceee49ab9b569abf6
Igifigi <ignacyswiderski@wp.pl>
2022-07-24 20:44:24 +0200

    initial commit

---
+++ background.js
@@ -0,0 +1,13 @@
+﻿var url = "";
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+  SetUrl();
+  if (!url.includes("www.cda.pl/video")) return;
+  chrome.tabs.executeScript(null, { file: "script.js" });
+});
+
+function SetUrl() {
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+    url = tabs[0].url;
+  });
+}
---
+++ script.js
@@ -0,0 +1,39 @@
+﻿function GetVideoHighestQuality(){
+    var outer_html = document.body.outerHTML
+    var index_of_quality = outer_html.indexOf('videoQuality') + 16
+    var quality = ''
+    while(
+        outer_html[index_of_quality] != 'p' && 
+        outer_html[index_of_quality + 1] != '"' && 
+        outer_html[index_of_quality + 2] != ',')
+    {
+        quality += outer_html[index_of_quality]
+        index_of_quality++
+    }
+    return quality
+}
+
+function GetCurrentUrl(){
+    var outer_html = document.body.outerHTML
+    var index_of_url = outer_html.indexOf('"url"') + 8
+    var url = ''
+    while(outer_html[index_of_url] != '"' && outer_html[index_of_url + 1] != ','){
+        url += outer_html[index_of_url]
+        index_of_url++
+    }
+    return url
+}
+
+function OpenUrl(url){
+    window.location.href = url
+}
+
+function UpgradeUrlToHigherQuality(url){
+    return url += '?wersja=' + GetVideoHighestQuality() + 'p'
+}
+
+function Reload(){
+    OpenUrl(UpgradeUrlToHigherQuality(GetCurrentUrl()))
+}
+
+Reload()
