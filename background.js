202b5b291d21de1144d5a6dcec6090178c9ee641
Ignacy Świderski <44808028+Igifigi@users.noreply.github.com>
2022-07-27 14:28:31 +0200

    Add files via upload

--- background.js
+++ background.js
@@ -1,13 +1,27 @@
 ﻿var url = "";
+var quality;
 
 chrome.browserAction.onClicked.addListener(function (tab) {
   SetUrl();
   if (!url.includes("www.cda.pl/video")) return;
-  chrome.tabs.executeScript(null, { file: "script.js" });
+  chrome.tabs.executeScript(tab.id, { file: "script.js" });
 });
 
 function SetUrl() {
   chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
     url = tabs[0].url;
   });
+}
+
+chrome.extension.onRequest.addListener(function (_quality) {
+  quality = _quality;
+  OpenUrl(UpgradeUrlToHigherQuality(url));
+});
+
+function OpenUrl(url) {
+  chrome.tabs.update(null, { url: url });
+}
+
+function UpgradeUrlToHigherQuality(url) {
+  return (url += "?wersja=" + quality);
 }
--- script.js
+++ script.js
@@ -1,39 +1,72 @@
 ﻿function GetVideoHighestQuality(){
     var outer_html = document.body.outerHTML
-    var index_of_quality = outer_html.indexOf('videoQuality') + 16
-    var quality = ''
-    while(
-        outer_html[index_of_quality] != 'p' && 
-        outer_html[index_of_quality + 1] != '"' && 
-        outer_html[index_of_quality + 2] != ',')
-    {
-        quality += outer_html[index_of_quality]
-        index_of_quality++
+
+    var quality = outer_html.match(new RegExp('(&quot;)(1080p)(&quot;)', 'gi')) || []
+    if(quality.length != 0){
+        quality = quality.map(function(str){
+            return str.substring(6, 11)
+        })
+        return quality[0]
     }
-    return quality
-}
-
-function GetCurrentUrl(){
-    var outer_html = document.body.outerHTML
-    var index_of_url = outer_html.indexOf('"url"') + 8
-    var url = ''
-    while(outer_html[index_of_url] != '"' && outer_html[index_of_url + 1] != ','){
-        url += outer_html[index_of_url]
-        index_of_url++
+    quality = outer_html.match(new RegExp('(&quot;)(720p)(&quot;)', 'gi')) || []
+    if(quality.length != 0){
+        quality = quality.map(function(str){
+            return str.substring(6, 10)
+        })
+        return quality[0]
     }
-    return url
+    quality = outer_html.match(new RegExp('(&quot;)(480p)(&quot;)', 'gi')) || []
+    if(quality.length != 0){
+        quality = quality.map(function(str){
+            return str.substring(6, 10)
+        })
+        return quality[0]
+    }
+    quality = outer_html.match(new RegExp('(&quot;)(360p)(&quot;)', 'gi')) || []
+    if(quality.length != 0){
+        quality = quality.map(function(str){
+            return str.substring(6, 10)
+        })
+        return quality[0]
+    }
+    
+
+    // var index_of_quality = outer_html.indexOf('videoQuality') + 16
+    // var quality = ''
+    // while(
+    //     outer_html[index_of_quality] != 'p' && 
+    //     outer_html[index_of_quality + 1] != '"' && 
+    //     outer_html[index_of_quality + 2] != ',')
+    // {
+    //     quality += outer_html[index_of_quality]
+    //     index_of_quality++
+    // }
+    // return quality
 }
 
-function OpenUrl(url){
-    window.location.href = url
-}
+// function GetCurrentUrl(){
+//     var outer_html = document.body.outerHTML
+//     var index_of_url = outer_html.indexOf('"url"') + 8
+//     var url = ''
+//     while(outer_html[index_of_url] != '"' && outer_html[index_of_url + 1] != ','){
+//         url += outer_html[index_of_url]
+//         index_of_url++
+//     }
+//     return url
+// }
 
-function UpgradeUrlToHigherQuality(url){
-    return url += '?wersja=' + GetVideoHighestQuality() + 'p'
-}
+// function OpenUrl(url){
+//     window.location.href = url
+// }
 
-function Reload(){
-    OpenUrl(UpgradeUrlToHigherQuality(GetCurrentUrl()))
-}
+// function UpgradeUrlToHigherQuality(url){
+//     return url += '?wersja=' + GetVideoHighestQuality() + 'p'
+// }
 
-Reload()
+// function Reload(){
+//     OpenUrl(UpgradeUrlToHigherQuality(Url))
+// }
+
+//Reload()
+var _quality = GetVideoHighestQuality()
+chrome.extension.sendRequest(_quality);
